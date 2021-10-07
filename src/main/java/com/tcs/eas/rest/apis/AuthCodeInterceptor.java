b104bfcb358a15693ee44b6920d5a82b015c4c48
manishsrivastava78 <manishsrivastava78@gmail.com>
2021-10-07 12:24:34 +0530

    Removed SOP statements

--- src/main/java/com/tcs/eas/rest/apis/AuthCodeInterceptor.java
+++ src/main/java/com/tcs/eas/rest/apis/AuthCodeInterceptor.java
@@ -12,19 +12,16 @@ public class AuthCodeInterceptor implements HandlerInterceptor {
 	   @Override
 	   public boolean preHandle(
 	      HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-	      System.out.println("hey manish");
-		   return true;
+	      return true;
 	   }
 	   @Override
 	   public void postHandle(
 	      HttpServletRequest request, HttpServletResponse response, Object handler, 
 	      ModelAndView modelAndView) throws Exception {
-		   System.out.println("hey manish1122");
-	   }
+		  }
 	   
 	   @Override
 	   public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
 	      Object handler, Exception exception) throws Exception {
-		   System.out.println("hey manishds");
-	   }
+		}
 	}
